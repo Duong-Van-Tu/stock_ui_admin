c613111683e04bcffd30b1874a4d81e12c8625f2
Duong Van Tu <dvtu.038@indivisys.jp>
2025-04-06 12:51:40 +0700

    fix UI count sentiment

--- src/components/count-sentiment/index.tsx
+++ src/components/count-sentiment/index.tsx
@@ -1,4 +1,7 @@
-import { Card, Col, Row } from 'antd';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+import { Col, Row, Collapse, Typography } from 'antd';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { useCallback, useEffect } from 'react';
 import {
@@ -8,6 +11,8 @@ import {
 import dayjs from 'dayjs';
 import { CountCard } from './count-card';
 import { useTranslations } from 'next-intl';
+
+const { Title } = Typography;
 
 type CountSentimentProps = {
   symbol: string;
@@ -48,37 +53,50 @@ export const CountSentiment = ({
   }, [fetchCountSentiment]);
 
   return (
-    <Card title='Count Sentiment'>
-      <Row gutter={[16, 16]}>
-        <Col span={24}>
-          <Row gutter={[16, 16]}>
-            <CountCard
-              title={t('positiveTitle')}
-              value={countSentiment.countPositive}
-              isPositive
-            />
-            <CountCard
-              title={t('veryPositiveTitle')}
-              value={countSentiment.countVeryPositive}
-              isPositive
-            />
-          </Row>
-        </Col>
-        <Col span={24}>
-          <Row gutter={[16, 16]}>
-            <CountCard
-              title={t('negativeTitle')}
-              value={countSentiment.countNegative}
-              isNegative
-            />
-            <CountCard
-              title={t('veryNegativeTitle')}
-              value={countSentiment.countVeryNegative}
-              isNegative
-            />
-          </Row>
-        </Col>
-      </Row>
-    </Card>
+    <Collapse defaultActiveKey={['1']} expandIconPosition='right'>
+      <Collapse.Panel
+        header={
+          <Title css={titleStyles} level={4}>
+            {t('countSentiment')}
+          </Title>
+        }
+        key='1'
+      >
+        <Row gutter={[16, 16]}>
+          <Col span={24}>
+            <Row gutter={[16, 16]}>
+              <CountCard
+                title={t('positiveTitle')}
+                value={countSentiment.countPositive}
+                isPositive
+              />
+              <CountCard
+                title={t('veryPositiveTitle')}
+                value={countSentiment.countVeryPositive}
+                isPositive
+              />
+            </Row>
+          </Col>
+          <Col span={24}>
+            <Row gutter={[16, 16]}>
+              <CountCard
+                title={t('negativeTitle')}
+                value={countSentiment.countNegative}
+                isNegative
+              />
+              <CountCard
+                title={t('veryNegativeTitle')}
+                value={countSentiment.countVeryNegative}
+                isNegative
+              />
+            </Row>
+          </Col>
+        </Row>
+      </Collapse.Panel>
+    </Collapse>
   );
 };
+
+const titleStyles = css`
+  margin-bottom: 0 !important;
+`;
