2d8993c8f55af1fa6e461d0ddc3b74bf2a64aab2
Duong Van Tu <17049351duongvantu@gmail.com>
2025-06-05 09:23:28 +0700

    comment logic middleware locale

--- src/constants/locale.constant.ts
+++ src/constants/locale.constant.ts
@@ -1,6 +1,6 @@
 import enUS from 'antd/es/locale/en_US';
-import viVN from 'antd/es/locale/vi_VN';
-import frFR from 'antd/es/locale/fr_FR';
+// import viVN from 'antd/es/locale/vi_VN';
+// import frFR from 'antd/es/locale/fr_FR';
 
 export enum Locale {
   EN = 'en'
