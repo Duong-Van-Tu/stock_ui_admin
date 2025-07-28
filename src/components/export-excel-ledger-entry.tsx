e37d71738a467f2d9aa4c6122af50ce6cf041909
Duong Van Tu <17049351duongvantu@gmail.com>
2025-07-28 15:40:27 +0700

    Fix export ledger entry

--- src/components/export-excel-ledger-entry.tsx
+++ src/components/export-excel-ledger-entry.tsx
@@ -47,111 +47,130 @@ export const ExportExcelLedgerEntry = ({
   };
 
   const handleExportLedger = async () => {
+    if (
+      !LedgerEntry.length ||
+      !Object.keys(cumulativeMap).length ||
+      !Object.keys(balanceMap).length
+    ) {
+      console.warn('Ledger data is empty or not yet loaded.');
+      return;
+    }
+
     setLoading(true);
 
-    const processedData = LedgerEntry.map((item) => {
-      const {
-        investmentCashIn = 0,
-        investmentCashOut = 0,
-        exitPrice = 0,
-        entryPrice = 0,
-        commission = 0,
-        entryDate,
-        exitDate
-      } = item;
+    try {
+      const processedData = LedgerEntry.map((item) => {
+        const {
+          investmentCashIn = 0,
+          investmentCashOut = 0,
+          exitPrice = 0,
+          entryPrice = 0,
+          commission = 0,
+          entryDate,
+          exitDate
+        } = item;
 
-      const winOrLoss =
-        investmentCashIn &&
-        investmentCashOut &&
-        investmentCashIn - investmentCashOut - commission;
+        const winOrLoss =
+          investmentCashIn &&
+          investmentCashOut &&
+          investmentCashIn - investmentCashOut - commission;
 
-      const plAmount =
-        investmentCashIn &&
-        investmentCashOut &&
-        investmentCashIn - investmentCashOut - commission;
+        const plAmount =
+          investmentCashIn &&
+          investmentCashOut &&
+          investmentCashIn - investmentCashOut - commission;
 
-      const plPercent = plAmount && (plAmount / investmentCashOut) * 100;
+        const plPercent = plAmount && (plAmount / investmentCashOut) * 100;
 
-      const cumulative = cumulativeMap[item.id];
-      const cumulativePercent =
-        cumulative && (cumulative / initialBalance) * 100;
+        const cumulative = cumulativeMap[item.id];
+        const cumulativePercent =
+          cumulative && (cumulative / initialBalance) * 100;
 
-      const balance = balanceMap[item.id];
+        const balance = balanceMap[item.id];
 
-      const stockPLPercent =
-        entryPrice &&
-        exitPrice &&
-        ((exitPrice - entryPrice) / entryPrice) * 100;
+        const stockPLPercent =
+          entryPrice &&
+          exitPrice &&
+          ((exitPrice - entryPrice) / entryPrice) * 100;
 
-      const holdingDays = calculateDIM(entryDate, exitDate);
+        const holdingDays = calculateDIM(entryDate, exitDate);
 
-      return {
-        Strategy: item.strategy || '-',
-        'Entry Date': item.entryDate ? formatDateTime(item.entryDate) : '-',
-        'Exit Date': formatDateTime(item.exitDate),
-        DIM: holdingDays,
-        Period: item.period || '-',
-        Symbol: item.symbol || '-',
-        'Win/Loss': winOrLoss ? (winOrLoss >= 0 ? 'Win' : 'Loss') : '-',
-        Action: item.action ? item.action : '-',
-        Strike: formatCurrency(item.strike),
-        Expiration: item.expiration
-          ? formatDateToDDMMYYYY(item.expiration)
-          : '-',
-        'Premium Paid': formatCurrency(item.premiumPaid),
-        'Premium Received': formatCurrency(item.premiumReceive),
-        'No. of Contracts': item.contracts ?? '-',
-        'Investment Cash Out': formatCurrency(item.investmentCashOut),
-        'Investment Cash In': formatCurrency(item.investmentCashIn),
-        Commission: formatCurrency(item.commission),
-        'P/L Amount': formatCurrency(plAmount),
-        'P/L (%)': formatPercent(plPercent),
-        'Cumulative Gain/Loss': formatCurrency(cumulative),
-        'Cumulative Gain/Loss (%)': formatPercent(cumulativePercent),
-        'Balance (5000$)': formatCurrency(balance),
-        'Entry Price': formatCurrency(item.entryPrice),
-        'Exit Price': formatCurrency(item.exitPrice),
-        'Stock P/L (%)': formatPercent(stockPLPercent),
-        Sector: item.sector || '-',
-        Notes: item.notes || '-'
-      };
-    });
+        return {
+          Strategy: item.strategy || '-',
+          'Entry Date': item.entryDate ? formatDateTime(item.entryDate) : '-',
+          'Exit Date': formatDateTime(item.exitDate),
+          DIM: holdingDays,
+          Period: item.period || '-',
+          Symbol: item.symbol || '-',
+          'Win/Loss': winOrLoss ? (winOrLoss >= 0 ? 'Win' : 'Loss') : '-',
+          Action: item.action ?? '-',
+          Strike: formatCurrency(item.strike),
+          Expiration: item.expiration
+            ? formatDateToDDMMYYYY(item.expiration)
+            : '-',
+          'Premium Paid': formatCurrency(item.premiumPaid),
+          'Premium Received': formatCurrency(item.premiumReceive),
+          'No. of Contracts': item.contracts ?? '-',
+          'Investment Cash Out': formatCurrency(item.investmentCashOut),
+          'Investment Cash In': formatCurrency(item.investmentCashIn),
+          Commission: formatCurrency(item.commission),
+          'P/L Amount': formatCurrency(plAmount),
+          'P/L (%)': formatPercent(plPercent),
+          'Cumulative Gain/Loss': formatCurrency(cumulative),
+          'Cumulative Gain/Loss (%)': formatPercent(cumulativePercent),
+          'Balance (5000$)': formatCurrency(balance),
+          'Entry Price': formatCurrency(item.entryPrice),
+          'Exit Price': formatCurrency(item.exitPrice),
+          'Stock P/L (%)': formatPercent(stockPLPercent),
+          Sector: item.sector || '-',
+          Notes: item.notes || '-'
+        };
+      });
 
-    const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('Ledger Entry');
-
-    const headers = Object.keys(processedData[0]);
-    worksheet.addRow(headers);
-
-    worksheet.getRow(1).eachCell((cell) => {
-      cell.font = { bold: true };
-    });
-
-    processedData.forEach((item) => {
-      worksheet.addRow(Object.values(item));
-    });
-
-    worksheet.columns.forEach((column) => {
-      if (column.values) {
-        const maxLength = Math.max(
-          ...column.values.map((v) => (v ? v.toString().length : 0))
-        );
-        column.width = maxLength + 2;
+      if (!processedData.length) {
+        console.warn('No data to export.');
+        return;
       }
-    });
 
-    const buffer = await workbook.xlsx.writeBuffer();
-    const blob = new Blob([buffer], {
-      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    });
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    const fileName = `Ledger_Entry_${dayjs()
-      .tz(TimeZone.NEW_YORK)
-      .format('MM-DD-YYYY_HH-mm')}.xlsx`;
-    link.download = fileName;
-    link.click();
-    setLoading(false);
+      const workbook = new ExcelJS.Workbook();
+      const worksheet = workbook.addWorksheet('Ledger Entry');
+
+      const headers = Object.keys(processedData[0]);
+      worksheet.addRow(headers);
+
+      worksheet.getRow(1).eachCell((cell) => {
+        cell.font = { bold: true };
+      });
+
+      processedData.forEach((item) => {
+        worksheet.addRow(Object.values(item));
+      });
+
+      worksheet.columns.forEach((column) => {
+        if (column.values) {
+          const maxLength = Math.max(
+            ...column.values.map((v) => (v != null ? v.toString().length : 0))
+          );
+          column.width = maxLength + 2;
+        }
+      });
+
+      const buffer = await workbook.xlsx.writeBuffer();
+      const blob = new Blob([buffer], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      const fileName = `Ledger_Entry_${dayjs()
+        .tz(TimeZone.NEW_YORK)
+        .format('MM-DD-YYYY_HH-mm')}.xlsx`;
+      link.download = fileName;
+      link.click();
+    } catch (error) {
+      console.error('Error exporting ledger:', error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
