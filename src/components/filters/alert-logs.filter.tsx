b6062f133bf44a59438bd5c5f4124f530bf0f40f
Duong-Van-Tu <17049351duongvantu@gmail.com>
2025-10-27 20:56:14 +0700

    fix filter

--- src/components/filters/alert-logs.filter.tsx
+++ src/components/filters/alert-logs.filter.tsx
@@ -102,12 +102,12 @@ export const AlertLogsFilter = ({
   const strategyLoading = useAppSelector(watchStrategyLoading);
   const latestEntryDate = useAppSelector(watchLatestEntryDate);
 
-  const defaultQuickRange: QuickRange = useMemo(() => {
-    if (!latestEntryDate) return 'today';
-    const latestNY = dayjs(latestEntryDate).tz(NY_TZ);
-    const todayNY = ny();
-    return latestNY.isSame(todayNY, 'day') ? 'today' : 'lastDay';
-  }, [latestEntryDate]);
+  // const defaultQuickRange: QuickRange = useMemo(() => {
+  //   if (!latestEntryDate) return 'today';
+  //   const latestNY = dayjs(latestEntryDate).tz(NY_TZ);
+  //   const todayNY = ny();
+  //   return latestNY.isSame(todayNY, 'day') ? 'today' : 'lastDay';
+  // }, [latestEntryDate]);
 
   const strategyOptions = useMemo(
     () => strategies?.map(({ id, name }) => ({ value: id, label: name })),
@@ -173,8 +173,10 @@ export const AlertLogsFilter = ({
   }, [symbol, form]);
 
   useEffect(() => {
-    handleQuickRangeChange(defaultQuickRange);
-  }, [defaultQuickRange, handleQuickRangeChange]);
+    handleQuickRangeChange('today');
+    // handleQuickRangeChange(defaultQuickRange);
+    // }, [defaultQuickRange, handleQuickRangeChange]);
+  }, [handleQuickRangeChange]);
 
   useEffect(() => {
     handleQuickRangeChange(form.getFieldValue('quickRange'));
