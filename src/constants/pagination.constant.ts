1bc2fe7e8ba055659955f1b9723be474a6c577f1
DuongTu <17049351duongvantu@gmail.com>
2025-12-29 22:51:30 +0700

    Fix render data wrong

--- src/constants/pagination.constant.ts
+++ src/constants/pagination.constant.ts
@@ -3,7 +3,7 @@ import { isMobile } from 'react-device-detect';
 export const PAGINATION = {
   total: 0,
   currentPage: 1,
-  pageSize: 10
+  pageSize: isMobile ? 20 : 100
 };
 
 export const PAGINATION_PARAMS = {
